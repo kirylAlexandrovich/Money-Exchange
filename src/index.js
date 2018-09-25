337667c6d192242f2c232ab3313435bee244b62b
Kiryl Alexandrovich <kiryl.alexandrovich@gmail.com>
2018-09-25 21:44:54 +0300

    money exchange func realization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const bank = [{
+        coin: 50,
+        count: 0,
+        name: 'H'
+    },{
+        coin: 25,
+        count: 0,
+        name: 'Q'
+    },{
+        coin: 10,
+        count: 0,
+        name: 'D'
+    },{
+        coin: 5,
+        count: 0,
+        name: 'N'
+    },{
+        coin: 1,
+        count: 0,
+        name: 'P'
+    }];
+
+    let i = 0;
+    const result = {};
+    if(currency > 0 && currency < 10000) {
+        while(i < bank.length) {
+            for(currency; currency >= bank[i].coin; currency -= bank[i].coin) {
+                bank[i].count++;
+                if(bank[i].count > 0){
+                    result[bank[i].name] = bank[i].count;
+                }
+            }
+            i++;
+        }
+        return result;
+    }
+    else if(currency >= 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};     
+    }
+    else {
+        return {};     
+    }
 }
